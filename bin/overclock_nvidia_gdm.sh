bd9032b175aaf9fadb2972eff0dc6f50ae829388
Matias Garcia <mdhgarcia@gmail.com>
2021-10-16 00:24:58 +0000

    Parameterize nvidia overclock script

--- bin/overclock_nvidia_gdm.sh
+++ bin/overclock_nvidia_gdm.sh
@@ -1,17 +1,45 @@
 #!/usr/bin/env bash
 
-# Default input parameters
-GPU_ID=0
-CORE_CLOCK=1450
-MEMORY_OFFSET=2000
-POWER_LIMIT=130
-FAN_SPEED=60
+usage() {
+	echo "Usage: overclock_nvidia.sh
+		-i  <gpu_index>        # Index of GPU (check output of nvidia-smi --list-gpus)
+		[-c  <core_clock>]     # Optional - Core clock speed in MHz (absolute)
+		[-m  <memory_offset>]  # Optional - Memory clock offset in MHz (double values from afterburner)
+		[-p  <power_limit>]    # Optional - Power limit in Watts
+		[-f  <fan_speed>]      # Optional - Fan speed (use 0 for default auto control)
+	" 1>&2 && exit 1
+}
 
-# Grab current GDM user ID and set up XAUTHORITY
-GDMUSER=`id -u gdm`
-export XAUTHORITY="/run/user/${GDMUSER}/gdm/Xauthority"
-export DISPLAY=:0
 
+# Process options
+while getopts ":i:c:m:p:f:" opt; do
+	case "${opt}" in
+		i)	GPU_ID="${OPTARG}"
+			;;
+		c)	CORE_CLOCK="${OPTARG}"
+			;;
+		m)	MEMORY_OFFSET="${OPTARG}"
+			;;
+		p)	POWER_LIMIT="${OPTARG}"
+			;;
+		f)	FAN_SPEED="${OPTARG}"
+			;;
+		*)	usage
+			;;
+	esac
+done
+
+[ "${1:-}" = "--" ] && shift
+
+# TODO: Sanity-check GPU_ID
+[ -z "${GPU_ID}" ] && echo "Must provide GPU index" && usage
+[ -z "${CORE_CLOCK}" ] \
+	&& [ -z "${MEMORY_OFFSET}" ] \
+	&& [ -z "${POWER_LIMIT}" ] \
+	&& [ -z "${FAN_SPEED}" ] \
+	&& echo "Nothing to do" && usage
+
+# Function definitions
 
 set_core_clock() {
 	# Set absolute clock speed
@@ -42,23 +70,36 @@ set_fan() {
 	fi
 }
 
-config_gpu() {
-	# Enable persistence mode
-	nvidia-smi -i "${1}" -pm 1
-	
-	set_core_clock "${1}" "${2}"
-	set_memory_offset "${1}" "${3}"
-	set_power_limit "${1}" "${4}"
-	set_fan "${1}" "${5}"
+# Attempt to find a serviceable XAUTHORITY
+find_x_config() {
+	export XAUTHORITY="$(ps -ax | grep Xorg | head -n 1 | sed 's|.*-auth *\(.*/Xauthority\).*|\1|')"
+	export DISPLAY="$(ps -axo pid= | xargs -I PID -r cat /proc/PID/environ 2> /dev/null | tr '\0' '\n' | grep ^DISPLAY=: | sort -u | sed 's|^DISPLAY=\(.*\)|\1|' | head -n1)"
 }
 
-# Display all GPUs
-# TODO: Use output to sanity-check arguments
-nvidia-smi --list-gpus
+# Start an Xorg server if there isn't one already
+find_x_config
+if [ -z "${XAUTHORITY}" ] || [ -z "${DISPLAY}" ]; then
+	Xorg&
+	find_x_config
+fi
+
+if [ -z "${XAUTHORITY}" ] || [ -z "${DISPLAY}" ]; then
+	echo "Fatal error: Couldn't find or start an X server."
+	echo "Ensure Xorg is installed and/or a display manager is running"
+	usage
+fi
+
+# Enable persistence mode
+nvidia-smi -i "${GPU_ID}" -pm 1
 
 # Configure GPU
-config_gpu "${GPU_ID}" "${CORE_CLOCK}" "${MEMORY_OFFSET}" "${POWER_LIMIT}" "${FAN_SPEED}"
+[ ! -z "${CORE_CLOCK}" ]    && set_core_clock    "${GPU_ID}" "${CORE_CLOCK}"
+[ ! -z "${MEMORY_OFFSET}" ] && set_memory_offset "${GPU_ID}" "${CORE_CLOCK}"
+[ ! -z "${POWER_LIMIT}" ]   && set_power_limit   "${GPU_ID}" "${POWER_LIMIT}"
+[ ! -z "${FAN_SPEED}" ]     && set_fan           "${GPU_ID}" "${FAN_SPEED}"
+
+# Find folder from which this script is run courtesy of https://stackoverflow.com/a/246128
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Query GPUs
-#nvidia-smi --query-gpu=timestamp,gpu_bus_id,utilization.gpu,utilization.memory,temperature.gpu,fan.speed,power.draw --format=csv
-nvidia-smi -i "${GPU_ID}" -q
+"${SCRIPT_DIR}"/get_gpu_status.sh
